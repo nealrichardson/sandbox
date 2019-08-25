ec44b55ea469ba61fd282cd229a7eb2a8ce0ddac
Neal Richardson <neal.p.richardson@gmail.com>
2019-08-24 21:49:40 -0700

    Try to catch stderr

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-FILE=$(ls foo*.bar)
+FILE=$(ls foo*.bar 2> /dev/null)
 if [ "$FILE" = "" ]; then
   echo "File not found"
 else
-  FILE=$(ls READM*)
+  FILE=$(ls READM* 2> /dev/null)
   echo $FILE
 fi
