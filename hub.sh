e06c8a80964bb419c5d681b4c18f89cd43d59a47
Neal Richardson <neal.p.richardson@gmail.com>
2019-09-04 19:55:21 -0700

    Try with pat

--- hub.sh
+++ hub.sh
@@ -4,4 +4,6 @@ AUTHOR_EMAIL=$(git log -1 --pretty=format:%ae)
 AUTHOR_NAME=$(git log -1 --pretty=format:%an)
 git config --global user.email "${AUTHOR_EMAIL}"
 git config --global user.name "${AUTHOR_NAME}"
+git clone -b ${TRAVIS_BRANCH} https://${GITHUB_PAT}@github.com/$TRAVIS_REPO_SLUG.git testdir
+cd testdir
 hub pull-request -m "testing PR"
