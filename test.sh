d5c212ecaf1c9292d911c0c798d76e4d4f2b9e54
Neal Richardson <neal.p.richardson@gmail.com>
2020-01-16 14:30:55 -0800

    Pare back shopt list

--- test.sh
+++ test.sh
@@ -4,11 +4,11 @@ set -x
 
 bash --version
 
-shopt -u complete_fullquote
-shopt -s expand_aliases
+# shopt -u complete_fullquote
+# shopt -s expand_aliases
 shopt -s extglob
-shopt -s globasciiranges
-shopt -s globstar
+# shopt -s globasciiranges
+# shopt -s globstar
 
 
 shopt
@@ -18,9 +18,6 @@ cd $APPVEYOR_BUILD_FOLDER
 touch file2.md
 
 ls
-find . -name "*.md"
-
-ls | grep -e "\.md"
 
 mkdir test
 cp *.md test
